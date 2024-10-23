e7358ba1be4619c39520b507e059a5886ef114cf
Aksanti Bahiga tacite <71480535+marcellintacite@users.noreply.github.com>
2024-10-23 08:55:56 +0200

    Update Axios.ts

--- libs/Axios.ts
+++ libs/Axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosCon = axios.create({
   // baseURL: "http://192.168.100.33:8000",
-  baseURL: "https://projetutor.onrender.com",
+  baseURL: "https://ratio-nais-desc-22-23.onrender.com",
 });
 
 export default axiosCon;
